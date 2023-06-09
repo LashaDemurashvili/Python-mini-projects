cfcfafb3bce48d2b771d3c117f044210d104e525
Lasha Demurashvili <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-09 21:30:39 +0400

    save_01

--- Project_2 multiplication table/multiplication_table.py
+++ Project_2 multiplication table/multiplication_table.py
@@ -1,1 +1,8 @@
-print("asas")
+# Multiplication table using for loop
+
+print(" Multiplication table ".center(40, "*"))
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i * j, end="\t")
+    print("\n")
+    # print()  #we can use only print also
