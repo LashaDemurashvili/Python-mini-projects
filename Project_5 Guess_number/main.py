0362d5954042ed6f816025e34df61d13ff7a1fac
Lasha Demurashvili <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-12 22:58:26 +0400

    game 0

--- Project_5 Guess_number/main.py
+++ Project_5 Guess_number/main.py
@@ -1,1 +1,19 @@
-# guess number
+# guess number - game
+
+import random
+
+comp_number = random.randint(1, 100)
+
+# print(comp_number)
+
+game_on = True
+steps = 0
+while game_on:
+    user_prompt = int(input("Please enter the whole number between 1-100\n: "))
+    steps += 1
+
+    if user_prompt == comp_number:
+        print("You win !!!")
+        print(f"{steps}")
+    elif user_prompt < comp_number:
+        print("Too height")
