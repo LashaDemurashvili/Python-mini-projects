a5979272a87cf7dd35f2fa85cfcd4b7fb2d4aaf2
Lasha Demurashvili <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-13 22:32:21 +0400

    Referee save_01

---
+++ Project_6 Python_Referee/main.py
@@ -0,0 +1,17 @@
+# comments here
+# comments here
+# comments here
+# comments here
+
+
+f1 = open("test_1.txt", "r")
+book_1 = f1.read()
+print(book_1)
+
+f2 = open("test_2.txt", "r")
+book_2 = f2.read()
+print(book_2)
+
+f1.close()
+
+f2.close()
