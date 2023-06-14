a45aa09ab8a72a8b920a9d52c857bf4ac99a522c
lasha <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-15 00:12:25 +0400

    save_00

---
+++ Project_7 Python_vision/test.py
@@ -0,0 +1,14 @@
+# test file
+# just test new linux
+# test file
+# just test new linux
+# test file
+# just test new linux
+# test file
+# just test new linux
+"""
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+"""
