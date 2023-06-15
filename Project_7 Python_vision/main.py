53dcdd400d24841c3c1c1c229d44cd6dbce93bbb
lasha <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-16 00:58:37 +0400

    save_01

--- Project_7 Python_vision/main.py
+++ Project_7 Python_vision/main.py
@@ -1,2 +1,36 @@
-for i in range(1,100):
-    print(i)
+import folium
+import requests
+from bs4 import BeautifulSoup  # process data
+
+r = requests.get("https://www.worldometers.info/coronavirus/")
+# <Response [404]> Bad
+# <Response [200]> Good
+
+# # receiving websites entire code
+c = r.content
+
+soup = BeautifulSoup(c, "html.parser")
+# print(soup)
+
+"""
+# find - finds only  first 
+# find_all - finds all  
+"""
+
+data = soup.find("tbody")
+rows = data.find_all("tr", {"style": ""})
+
+print(rows[1])
+
+
+"""
+map = folium.Map(location=[32.2248, 46.626], zoom_start=6, tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Markers")
+
+for item in [[32.21, 46.92], [32.32, 46.72], [32.92, 46.12]]:
+    fg.add_child(folium.Marker(location=item, popup="Testing"))
+
+map.add_child(fg)
+map.save("CoronaMap.html")
+"""
--- Project_7 Python_vision/test.py
+++ Project_7 Python_vision/test.py
@@ -1,15 +1,25 @@
-# test file
-# just test new linux
-# test file
-# just test new linux
-# test file
-# just test new linux
-# test file
-# just test new linux
-"""
-
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-
-"""
-print("As")
+/usr/bin/python3.10 /home/lasha/Python-mini-projects/Project_7 Python_vision/main.py 
+<tr style="">
+<td style="font-size:12px;color: grey;text-align:center;vertical-align:middle;">1</td>
+<td style="font-weight: bold; font-size:15px; text-align:left;"><a class="mt_a" href="country/us/">USA</a></td>
+<td style="font-weight: bold; text-align:right">107,198,306</td>
+<td style="font-weight: bold; text-align:right;"></td>
+<td style="font-weight: bold; text-align:right;">1,166,864                                </td>
+<td style="font-weight: bold; 
+                                    text-align:right;"></td>
+<td style="font-weight: bold; text-align:right">105,338,874</td>
+<td style="font-weight: bold; text-align:right;"></td>
+<td style="text-align:right;font-weight:bold;">692,568</td>
+<td style="font-weight: bold; text-align:right">843</td>
+<td style="font-weight: bold; text-align:right">320,181</td>
+<td style="font-weight: bold; text-align:right">3,485</td>
+<td style="font-weight: bold; text-align:right">1,180,568,581</td>
+<td style="font-weight: bold; text-align:right">3,526,135</td>
+<td style="font-weight: bold; text-align:right"><a href="/world-population/us-population/">334,805,269</a> </td>
+<td data-continent="North America" style="display:none">North America</td>
+<!-- 1 Case/Death/Test every X -->
+<td>3</td><td>287</td><td>0</td>
+<td style="font-weight: bold; text-align:right"></td>
+<td style="font-weight: bold; text-align:right"></td>
+<td style="font-weight: bold; text-align:right">2,069</td>
+</tr>
