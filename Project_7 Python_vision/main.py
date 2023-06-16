f63ce39db22d0949e57dc901e1845cde2ae799ed
lasha <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-16 23:48:07 +0400

    save_11

--- Project_7 Python_vision/main.py
+++ Project_7 Python_vision/main.py
@@ -7,16 +7,13 @@ r = requests.get("https://www.worldometers.info/coronavirus/")
 # <Response [404]> Bad
 # <Response [200]> Good
 
-# # receiving websites entire code
+# # receiving websites entire code (contents)
 c = r.content
 
 soup = BeautifulSoup(c, "html.parser")
-# print(soup)
-
-
+# parsing as html document
 # find - finds only  first
 # find_all - finds all
-
 
 data = soup.find("tbody")
 rows = data.find_all("tr", {"style": ""})
@@ -29,10 +26,6 @@ for item in rows:
 
     data_dict[c_name] = int(c_cases.replace(",", ""))
 
-    # data_dict = {c_name, c_cases}
-    # key = c_name
-    # value = c_cases
-    # data_dict.update({key: value})
 
 print(data_dict)
 
@@ -43,14 +36,15 @@ lat = list(cdata["latitude"])
 lon = list(cdata["longitude"])
 cnt = list(cdata["name"])
 
-map = folium.Map(location=[32.2248, 46.626], zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location=[42.42, 42.42], zoom_start=3, tiles="Stamen "
+                                                                                                            "Terrain")
 
 fg = folium.FeatureGroup(name="My Markers")
 
 
-
 def radius_gen(radius):
     return radius ** 0.20
+
 
 def color_gen(num):
     if num < 10000:
@@ -64,12 +58,16 @@ def color_gen(num):
     else:
         return "red"
 
+
 for lt, ln, ct in zip(lat, lon, cnt):
 
     if ct in data_dict.keys():
-        fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(ct) + "\n" + str(data_dict[ct]),
-                                         radius=radius_gen(data_dict[ct]),
-                                         fill_color=color_gen(data_dict[ct]), color="#cc1e0e", fill_opacity=0.5))
+        try:
+            fg.add_child(folium.CircleMarker(location=[lt, ln], popup=str(ct) + "\n" + str(data_dict[ct]),
+                                             radius=radius_gen(data_dict[ct]),
+                                             fill_color=color_gen(data_dict[ct]), color="#cc1e0e", fill_opacity=0.5))
+        except:
+            pass
 
 map.add_child(fg)
 map.save("CoronaMap.html")
