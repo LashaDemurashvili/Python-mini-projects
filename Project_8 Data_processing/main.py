e8cdef23ca2d2a075c183c9e440c779339a1f2f9
lasha <lasha.demurashvili.1@iliauni.edu.ge>
2023-06-17 01:31:09 +0400

    proj8 "save_00"

--- Project_8 Data_processing/main.py
+++ Project_8 Data_processing/main.py
@@ -3,4 +3,13 @@
 
 
 """
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from openpyxl.styles import *
+from openpyxl.worksheet.table import Table, TableStyleInfo
 
+
+r = requests.get("https://www.worldometers.info/world-population/")
+
+print(r)
